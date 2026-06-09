0bdacf26e1fa2e302a9ba002ca06a393d76466b4
unknown <20241pvai0030031@estudantes.ifpr.edu.br>
2026-06-09 09:52:38 -0300

    atualizadoooo

--- MeuJogo.py
+++ MeuJogo.py
@@ -1,5 +1,8 @@
 import arcade
 
+Altura = 600
+Largura = 800
+Titulo = "Meu Jogo"
 #personagem
 class Player(arcade.Sprite):
     def __init__(self):
@@ -15,22 +18,25 @@ class Player(arcade.Sprite):
         elif self.change_x < 0:
             self.texture = self.textura_esquerda
 
-        if self.center_x > 800:
+        #parar o personagem antes dele sair da tela
+        if(self.right > Largura):
             self.change_x = 0
-           
-        elif self.center_x < 0:
+            self.right = Largura
+        elif(self.left < 0):
             self.change_x = 0
-
-        if self.center_y > 600:
+            self.left = 0
+        
+        if(self.top > Altura):
             self.change_y = 0
-            
-        elif self.center_y < 0:
+            self.top = Altura
+        elif(self.bottom < 0):
             self.change_y = 0
+            self.bottom = 0
        
 
 class Moeda(arcade.Sprite):
     def __init__(self):
-        super().__init__("moeda.png", scale=0.20)
+        super().__init__("banana.png", scale=0.10)
 
     #atualizar personagem
     def update(self, delta_time):
@@ -38,42 +44,46 @@ class Moeda(arcade.Sprite):
         self.center_x += self.change_x
         self.center_y += self.change_y
 
-        if self.right > 800:
-            self.change_x = 0
+        if self.right > 800 or self.left <0:
+            self.change_x *= -1
            
-        elif self.left < 0:
-            self.change_x = 0
 
-        if self.top > 600:
-            self.change_y = 0
-            
-        elif self.bottom < 0:
-            self.change_y = 0
+        elif self.top > 600 or self.bottom < 0:
+            self.change_y *= -1
+    
        
 
 class JanelaJogo(arcade.Window):
     def __init__(self):
-         super().__init__(800, 600, "Meu Jogo")
-         #cor RGB (vermelho, verde, azul)
-         arcade.set_background_color((100, 10, 70))
-         #definir velocidade   
-         self.movimento = 3
+        super().__init__(800, 600, "Meu Jogo")
+        #cor RGB (vermelho, verde, azul)
+        arcade.set_background_color((100, 10, 70))
+        #definir velocidade   
+        self.movimento = 3
         #criar personagem
-         self.jogador = Player()
-         self.moeda = Moeda()
-         #posicionar ele na tela
-         self.jogador.center_x = 400
-         self.jogador.center_y = 300
-         self.moeda.center_x = 500
-         self.moeda.center_y = 300
-         self.moeda.change_x = self.movimento
-         self.moeda.change_y = self.movimento
+        self.jogador = Player()
+        self.moeda = Moeda()
+        #posicionar ele na tela
+        self.jogador.center_x = 400
+        self.jogador.center_y = 300
+        self.moeda.center_x = 500
+        self.moeda.center_y = 300
+        self.moeda.change_x = self.movimento
+        self.moeda.change_y = self.movimento
 
-         self.sprite_jogador = arcade.SpriteList()
-         self.sprite_jogador.append(self.jogador)
+        self.moeda2 = Moeda()
+        self.moeda2.left = 0
+        self.moeda2.bottom = 300
 
-         self.sprite_moeda = arcade.SpriteList()
-         self.sprite_moeda.append(self.moeda)
+        self.moeda3 = Moeda()
+        self.moeda3.left = 700
+        self.moeda3.bottom = 150
+
+        self.sprite_jogador = arcade.SpriteList()
+        self.sprite_jogador.append(self.jogador)
+
+        self.sprite_moeda = arcade.SpriteList()
+        self.sprite_moeda.append(self.moeda)
 
     def on_update(self, delta_time):
     
@@ -86,6 +96,24 @@ class JanelaJogo(arcade.Window):
         #desenhar meu personagem
         self.sprite_jogador.draw()
         self.sprite_moeda.draw()
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.RIGHT: 
+            self.jogador.change_x += self.movimento
+        if key == arcade.key.LEFT:
+            self.jogador.change_x -= self.movimento
+        if key == arcade.key.UP:
+            self.jogador.change_y += self.movimento
+        if key == arcade.key.DOWN:
+            self.jogador.change_y -= self.movimento
+        if key == arcade.key.ESCAPE:
+            arcade.close_window()
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.RIGHT or key == arcade.key.LEFT: 
+            self.jogador.change_x = 0
+        elif key == arcade.key.UP or key == arcade.key.DOWN:
+            self.jogador.change_y = 0     
  
 def main():
     jogo = JanelaJogo()
